293452d6aa121e8af20db11fdf81fdd7172db4e2
Rafael Sousa <104471811+RafaelSousa01@users.noreply.github.com>
2022-05-25 22:53:36 -0300

    Adicionar arquivos de projeto.

---
+++ Course/Entities/ImportedProduct.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace Course.Entities
+{
+    class ImportedProduct : Product
+    {
+
+        public double CustomFee { get; set; }
+
+        public ImportedProduct()
+        {
+
+        }
+
+        public ImportedProduct(string name, double price, double customFee)
+            : base(name, price)
+        {
+            CustomFee = customFee;
+        }
+
+        public double TotalPrice()
+        {
+            return CustomFee + Price;
+        }
+
+
+        public override string PriceTag()
+        {
+            return $"{Name} $ {TotalPrice().ToString("F2")} (Customs fee: $ { CustomFee.ToString("F2")}";
+        }
+
+
+
+    }
+}
---
+++ Course/Entities/UsedProduct.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Entities
+{
+    class UsedProduct : Product
+    {
+        public DateTime Manufacture { get; set; }
+
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(string name, double price,DateTime manufacture)
+            : base (name, price)
+        {
+            Manufacture = manufacture;
+        }
+
+        public override string PriceTag()
+        {
+            return $"{Name} (used) $ {Price.ToString("F2")} (Manufactured date: {Manufacture.ToString("dd/MM/yyyy")}";
+
+        }
+
+
+
+    }
+}
